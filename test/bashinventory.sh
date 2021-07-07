a874eaf82cf5dac227ffd77669afea233b064bc5
Anthony <anlaniesse.pro@gmail.com>
2021-07-07 10:32:06 +0200

    Fichier bash modifié

--- test/bashinventory.sh
+++ test/bashinventory.sh
@@ -1,16 +1,25 @@
 #!/bin/bash 
 
+#If result.txt is not empty, clean it#
+result="result.txt"
+if (test -s $result)
+  then 
+    > $result
+fi
 
-###Find IP Address###
-a=$(nmap -sP 192.168.6.0/24 |grep "192.168.6." |awk '{print $6}' |sed 's/(//g' |sed 's/)//g')
+###Find IP Address and stock it into a variable###
+ip=$(nmap -sP 192.168.6.0/24 |grep "192.168.6." |awk '{print $6}' |sed 's/(//g' |sed 's/)//g')
 
-echo $a 
+echo $ip
 
-###Find Mac address###
-for param in $a
+###Find Mac address using result of the variable before###
+#For each value in a, search and stock the mac address#
+for param in $ip
   do
-    param=sudo arping -c 1 $a 
-    echo $param
+    #search mac address and result only it#
+    mac=$(sudo arping -c 1 $param |grep "bytes" |awk '{print $4}')
+    #Stock this result on a list
+    list=($param,$mac)
+    
+    echo ${list[*]} >> $result
   done
-#mac=$(sudo arping -c 1 192.168.6.2)
-#echo $mac
