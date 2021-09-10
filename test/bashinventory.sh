60ca69aeb0de203f887180cfce71c86ac524e186
Anthony <anlaniesse.pro@gmail.com>
2021-09-10 14:36:42 +0200

    Correction doublons @Mac

--- test/bashinventory.sh
+++ test/bashinventory.sh
@@ -8,18 +8,25 @@ if (test -s $result)
 fi
 
 ###Find IP Address and stock it into a variable###
-ip=$(nmap -sP 192.168.6.0/24 |grep "192.168.6." |awk '{print $6}' |sed 's/(//g' |sed 's/)//g')
+ip=$(nmap -sP 192.168.6.0/24 |grep "192.168.6." |awk '{print $NF}' |sed 's/(//g' |sed 's/)//g')
 
 echo $ip
 
-###Find Mac address using result of the variable before###
-#For each value in a, search and stock the mac address#
+###Find MAC Address using result of the variable ip###
+#For each value in ip, search and stock the mac address#
 for param in $ip
   do
-    #search mac address and result only it#
-    mac=$(sudo arping -c 1 $param |grep "bytes" |awk '{print $4}')
+    echo "Traitement de $ip"
+    #search MAC Address and result only it#
+    mac=$(sudo arping -c 1 $param |grep "bytes" |awk '{print $4}' |head -n 1)
+    vendor=$(curl -s "http://www.macvendorlookup.com/api/v2/{$mac}" |jq '.[].company')
     #Stock this result on a list
-    list=($param,$mac)
+    list=($param,$mac,$vendor)
     
     echo ${list[*]} >> $result
   done
+
+
+### Use MAC Address to ask API Rest informations ###
+#"""utiliser la variable mac qui se trouve dans for soit utiliser le resultat de la première colonne de result.txt"""
+
