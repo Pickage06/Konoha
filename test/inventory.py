d1f83829affaad8afb888e91da8330ffeaadfb4f
Anthony <anlaniesse.pro@gmail.com>
2021-09-13 15:34:24 +0200

    Modification du fichier inventory.py

--- test/inventory.py
+++ test/inventory.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import nmap
-
+import nmap3
+#pip3 install python-nmap
+import os 
 ######## INVENTORY HARDWARE ########
 """The objective of the script is to make an inventory
  of all the equipments on a factory. To do that, please observe 
@@ -9,14 +10,34 @@ import nmap
 
 
 ######TODO######
-###Find IP Address###
-ip = nmap -sP 192.168.6.0/24 |grep "192.168.6." |awk '{print $6}' |sed 's/(//g' |sed 's/)//g'
-print(ip)
-###Find MAC Address###
-"""With NMAP or PING + ARP or AWK"""
+#If result.txt is not empty, clean it#
+def empty_file():
+  empty_file = os.stat("result.txt").st_size == 0
+  if empty_file == False:
+    open("result.txt", "w").close()
 
+###Find IP Address###
+#Do a nmap in your subnet#
+nm = nmap3.Nmap()
+result = nm.nmap_subnet_scan("192.168.6.0/24")
+
+#For all key in the dico, show them#
+#for key in result.keys():
+#  print(key)
+
+#Extract the IPs address of the nmap and Stock it into a list#
+#Extract keys associated to ip address#
+ips=list(result.keys())[:1] 
+#print(ip) >> result.txt
+print(*ips, sep = ',')
+
+###Find MAC Address###
+def arping():
+  for ip in ips():
+    arpreq.arpreq(ip)
+
+print(arping)
 ##Ping address##"+ list of only IP Address"
-##Extract MAC address##
 ##Stock all informations##
 "{dict}" "+ list of only IP Address"
 ###Find API Constructor###
